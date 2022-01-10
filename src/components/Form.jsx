6293fe1100e82b1adc1f6a7b95d6f2635936ec50
Nelson Alvarez <nalvare84523@universidadean.edu.co>
2022-01-10 16:29:29 -0500

    Change of input form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,57 +20,66 @@ function Form() {
     input.addEventListener("blur", rem);
   }); */
 
-  const [getFocus, setFocus] = useState(false);
-/*   const [getBlur, setBlur] = useState(true); */
 
-  const focus = () => {
-    if(getFocus === false) {
-      setFocus(true);
-    } else {
-      setFocus(false);
-    }
-  }
+/*   const input = useRef(null);
+  const container = useRef(null);
+  const inputB = input.current;
+  const containerB = container.current; */
+
+  const [getFocus, setFocus] = useState(false);
+  const [getName, setName] = useState("");
+  const [getEmail, setEmail] = useState("");
+  const [getCompany, setCompany] = useState("");
+  const [getCountry, setCountry] = useState("");
+  const [getPhone, setPhone] = useState("");
+  const [getMessage, setMessage] = useState("");
+/*const [getBlur, setBlur] = useState(true);*/
+
+
+
+
+
 
   return (
     <form className="form-vass bg-black">
       <div className="d-flex flex-column flex-lg-row">
         <div className="col-lg-5 d-flex flex-column">
-          <div className={getFocus ? "container-form focus" : "container-form"}>
+          <div className={getFocus === "nombre" || getName.length > 0 ? "container-form focus" : "container-form"}>
             <div className="div">
               <h5>Nombres y apellidos</h5>
-              <input onFocus={focus} onBlur={focus} type="text" className="input" />
+              <input name="nombre" onChange={(e) => setName(e.target.value)} onFocus={() => setFocus("nombre")} onBlur={() => setFocus("")} type="text" className="input" />
             </div>
           </div>
-          <div className={getFocus ? "container-form focus" : "container-form"}>
+          <div className={getFocus === "correo" || getEmail.length > 0 ? "container-form focus" : "container-form"}>
             <div className="div">
               <h5>Email</h5>
-              <input onFocus={focus} onBlur={focus} type="email" className="input" />
+              <input name="correo" onChange={(e) => setEmail(e.target.value)} onFocus={() => setFocus("correo")} onBlur={() => setFocus("")} type="email" className="input" />
             </div>
           </div>
-          <div className={getFocus ? "container-form focus" : "container-form"}>
+          <div className={getFocus === "empresa" || getCompany.length > 0 ? "container-form focus" : "container-form"}>
             <div className="div">
               <h5>Empresa/Organismo</h5>
-              <input onFocus={focus} onBlur={focus} type="text" className="input" />
+              <input name="empresa" onChange={(e) => setCompany(e.target.value)} onFocus={() => setFocus("empresa")} onBlur={() => setFocus("")} type="text" className="input" />
             </div>
           </div>
-          <div className={getFocus ? "container-form focus" : "container-form"}>
+          <div className={getFocus === "pais" || getCountry.length > 0 ? "container-form focus" : "container-form"}>
             <div className="div">
               <h5>Pais</h5>
-              <input onFocus={focus} onBlur={focus} type="text" className="input" />
+              <input name="pais" onChange={(e) => setCountry(e.target.value)} onFocus={() => setFocus("pais")} onBlur={() => setFocus("")} type="text" className="input" />
             </div>
           </div>
         </div>
         <div className="col-lg-5 d-flex flex-column">
-          <div className={getFocus ? "container-form focus" : "container-form"}>
+          <div className={getFocus === "telefono" || getPhone.length > 0 ? "container-form focus" : "container-form"}>
             <div className="div">
               <h5>Telefono</h5>
-              <input onFocus={focus} onBlur={focus} type="phone" className="input" />
+              <input name="telefono" onChange={(e) => setPhone(e.target.value)} onFocus={() => setFocus("telefono")} onBlur={() => setFocus("")} type="phone" className="input" />
             </div>
           </div>
-          <div className={getFocus ? "container-form focus" : "container-form"}>
+          <div className={getFocus === "mensaje" || getMessage.length > 0 ? "container-form focus" : "container-form"}>
             <div className="div">
               <h5>Mensaje</h5>
-              <input onFocus={focus} onBlur={focus} type="text" className="input" />
+              <input name="mensaje" onChange={(e) => setMessage(e.target.value)} onFocus={() => setFocus("mensaje")} onBlur={() => setFocus("")} type="text" className="input" />
             </div>
           </div>
           <div>
